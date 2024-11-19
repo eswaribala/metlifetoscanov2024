9a52c0287cd3f15c86ad2d1d0be84db523973dbf
Parameswari Bala <parameswaribala@gmail.com>
2024-11-19 15:29:48 +0530

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,28 @@
-alert("started rocking java script");
+//alert("started rocking java script");
+
+document.addEventListener('load',function(){
+
+    document.addEventListener('submit',function(){
+        let firstName=document.querySelector("#firstName").value;
+        let lastName=document.querySelector("#lastName").value;
+        let dob=document.querySelector("#dob").value;
+        let gender=document.querySelector("#gender").value;
+        let selectedOccupation=document
+            .querySelector('input[name="occupation"]:checked');
+        let occupation="";
+        if(selectedOccupation){
+            occupation=selectedOccupation.value;
+        }
+
+        let customerData={
+            "firstName":firstName,
+            "lastName":lastName,
+            "dob":dob,
+            "gender":gender,
+            "occupation":occupation
+        }
+        alert(customerData);
+
+    })
+
+})
